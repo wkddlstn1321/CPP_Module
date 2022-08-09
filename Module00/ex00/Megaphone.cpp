6a97f70ac401a0822875d15f77490e82fe510fc9
wkddlstn1321 <wkddlstn132@gmail.com>
2022-08-09 20:36:20 +0900

    start

---
+++ Module00/ex00/Megaphone.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<ctype.h>
+
+int	main(int ac, char **av)
+{
+	(void)av;
+	if (ac < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1 ; i < ac ; i++) {
+			for (int j = 0 ; av[i][j] ; j++) {
+				std::cout << (char)toupper(av[i][j]);
+			}
+			std::cout << " ";
+		}
+		std::cout << std::endl;
+	}
+}
