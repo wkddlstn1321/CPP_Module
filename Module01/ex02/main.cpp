81b2203ad0258a582749f3502fe287b2d1335793
wkddlstn1321 <wkddlstn132@gmail.com>
2022-09-05 14:23:33 +0900

    mod01 ex02 clear

---
+++ Module01/ex02/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int	main(void)
+{
+	std::string	brain = "HI THIS IS BRAIN";
+	std::string	*brainPTR = &brain;
+	std::string	&brainREF = brain;
+
+	std::cout << "brain    address : " << &brain << std::endl;
+	std::cout << "brainPTR address : " << &brainPTR << std::endl;
+	std::cout << "brainREF address : " << &brainREF << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "brain    value : " << brain << std::endl;
+	std::cout << "brainPTR value : " << *brainPTR << std::endl;
+	std::cout << "brainREF value : " << brainREF << std::endl;
+}
