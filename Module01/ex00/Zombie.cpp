84b67fffeb620fc0e59aa095828588d5283c572b
wkddlstn1321 <wkddlstn132@gmail.com>
2022-09-05 13:16:18 +0900

    ex01 00 clear

--- Module01/ex00/Zombie.cpp
+++ Module01/ex00/Zombie.cpp
@@ -2,5 +2,15 @@
 
 void	Zombie::announce(void)
 {
-	std::cout << this->name << "BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie::Zombie(std::string name)
+{
+	this->name = name;
+}
+
+Zombie::~Zombie(void)
+{
+	std::cout << this->name << " is delete" << std::endl;
 }
--- Module01/ex00/main.cpp
+++ Module01/ex00/main.cpp
@@ -2,7 +2,14 @@
 
 int	main(void)
 {
-	Zombie zombie;
+	Zombie	zombie("Foo");
+	Zombie	zombie2("Foo2");
+	Zombie	*zombie3;
 
-	
+	zombie.announce();
+	zombie2.announce();
+	zombie3 = newZombie("newZombie");
+	randomChump("randomChump");
+	zombie3->announce();
+	delete(zombie3);
 }
