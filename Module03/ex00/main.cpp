14bc0fb0a1312851f80dfe5ed4bca6969a3a923a
wkddlstn1321 <wkddlstn132@gmail.com>
2022-09-09 18:25:35 +0900

    main add \n

--- Module03/ex00/main.cpp
+++ Module03/ex00/main.cpp
@@ -5,14 +5,22 @@ int	main(void)
 	ClapTrap	a("a");
 	ClapTrap	b("b");
 
+	std::cout << std::endl;
 	a.setAttackDamage(5);
 	b.setAttackDamage(10);
 	a.attack("b");
+	std::cout << std::endl;
 	b.takeDamage(a.getAttackDamage());
+	std::cout << std::endl;
 	b.beRepaired(2);
+	std::cout << std::endl;
 	a.attack("b");
+	std::cout << std::endl;
 	b.takeDamage(a.getAttackDamage());
+	std::cout << std::endl;
 	b.attack("a");
+	std::cout << std::endl;
 	a.takeDamage(b.getAttackDamage());
+	std::cout << std::endl;
 	a.beRepaired(10);
 }
