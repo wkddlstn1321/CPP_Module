45a1401b8ad9d4dc245d404f86df597f8bfb56f8
wkddlstn1321 <wkddlstn132@gmail.com>
2022-09-11 20:06:46 +0900

    why mem_fun_ref error?

---
+++ Module00/ex02/Account.cpp
@@ -0,0 +1,119 @@
+#include"Account.hpp"
+#include<ctime>
+#include<iostream>
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account()
+{
+}
+
+Account::Account( int initial_deposit )
+{
+	_displayTimestamp();
+	this->_accountIndex = _nbAccounts;
+	this->_amount = initial_deposit;
+	_totalAmount += initial_deposit;
+	_nbAccounts += 1;
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "created" << std::endl;
+}
+
+Account::~Account()
+{
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "closed" << std::endl;
+}
+
+void	Account::displayAccountsInfos( void )
+{
+	_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts() << ";";
+	std::cout << "total:" << getTotalAmount() << ";";
+	std::cout << "deoisuts:" << getNbDeposits() << ";";
+	std::cout << "withdrawals:" << getNbWithdrawals() << std::endl; 
+}
+
+void	Account::_displayTimestamp( void )
+{
+	time_t	timer = time(NULL);
+	struct tm	*t = localtime(&timer);
+	char	buf[16];
+
+	strftime(buf, 16, "%Y%m%d_%H%M%S", t);
+	std::cout << "[" << buf << "] ";
+}
+
+int	Account::getNbAccounts( void )
+{
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount( void )
+{
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits( void )
+{
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals( void )
+{
+	return (_totalNbWithdrawals);
+}
+
+void	Account::displayStatus( void ) const
+{
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "deposits:" << this->_nbDeposits << ";";
+	std::cout << "withdrawals:" << this->_nbWithdrawals << ";" << std::endl;
+}
+
+void	Account::makeDeposit( int deposit )
+{
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "p_amount:" << this->_amount << ";";
+	std::cout << "deposit:" << deposit << ";";
+	this->_amount += deposit;
+	_totalAmount += deposit;
+	std::cout << "amount:" << this->_amount << ";";
+	_totalNbDeposits += 1;
+	this->_nbDeposits += 1;
+	std::cout << "nb_deposits:" << this->_nbDeposits << ";" << std::endl;
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	if (this->_amount >= withdrawal)
+	{
+		std::cout << "index:" << this->_accountIndex << ";";
+		std::cout << "p_amount:" << this->_amount << ";";
+		std::cout << "withdrawals:" << withdrawal << ";";
+		_totalAmount -= withdrawal;
+		this->_amount -= withdrawal;
+		std::cout << "amount:" << this->_amount << ";";
+		this->_nbWithdrawals += 1;
+		_totalNbWithdrawals += 1;
+		std::cout << "nb_withdrawals:" << this->_nbWithdrawals << ";" << std::endl;
+	}
+	else
+	{
+		std::cout << "index:" << this->_accountIndex << ";";
+		std::cout << "p_amount:" << this->_amount << ";";
+		std::cout << "withdrawal:" << "refused" << std::endl;
+	}
+}
+
+int		Account::checkAmount( void ) const
+{
+}
