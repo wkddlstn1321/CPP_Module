0dd2ff7b1f6b04e5c2c0d21fe0cf77d170acc8ee
wkddlstn1321 <wkddlstn132@gmail.com>
2022-09-11 21:34:26 +0900

    m1 problem?

--- Module00/ex00/Megaphone.cpp
+++ Module00/ex00/Megaphone.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
+#include<algorithm>
 #include<ctype.h>
+#include<string.h>
 
 int	main(int ac, char **av)
 {
@@ -8,10 +10,14 @@ int	main(int ac, char **av)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
+		int i = 0;
 		for (int i = 1 ; i < ac ; i++) {
-			for (int j = 0 ; av[i][j] ; j++) {
-				std::cout << (char)toupper(av[i][j]);
-			}
+			std::string	str = av[i];
+			std::transform(str.begin(), str.end(), str.begin(), std::tolower);
+			std::cout << str << std::endl;
+			// for (int j = 0 ; av[i][j] ; j++) {
+			// 	std::cout << (char)toupper(av[i][j]);
+			// }
 			std::cout << " ";
 		}
 		std::cout << std::endl;
