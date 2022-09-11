03570b2e2af9faac324aec8f6bebc17d2499b20a
wkddlstn1321 <wkddlstn132@gmail.com>
2022-09-11 15:45:50 +0900

    feat : comment + \n

--- Module01/ex05/Harl.cpp
+++ Module01/ex05/Harl.cpp
@@ -26,17 +26,17 @@ void	Harl::complain(std::string name)
 
 void	Harl::debug()
 {
-	std::cout << "DEBUG\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+	std::cout << "DEBUG\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger.\nI really do!" << std::endl;
 }
 
 void	Harl::info()
 {
-	std::cout << "INFO\nI cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << "INFO\nI cannot believe adding extra bacon costs more money.\nYou didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void	Harl::warning()
 {
-	std::cout << "WARNING\nI think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << "WARNING\nI think I deserve to have some extra bacon for free.\nI’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void	Harl::error()
