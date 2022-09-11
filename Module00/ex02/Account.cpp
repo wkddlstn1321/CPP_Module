1cbf65af4fe7e2949ebf5597a48eba151c4fa11e
wkddlstn1321 <wkddlstn132@gmail.com>
2022-09-11 20:23:09 +0900

    default constructor del

--- Module00/ex02/Account.cpp
+++ Module00/ex02/Account.cpp
@@ -7,10 +7,6 @@ int	Account::_totalAmount = 0;
 int	Account::_totalNbDeposits = 0;
 int	Account::_totalNbWithdrawals = 0;
 
-Account::Account()
-{
-}
-
 Account::Account( int initial_deposit )
 {
 	_displayTimestamp();
