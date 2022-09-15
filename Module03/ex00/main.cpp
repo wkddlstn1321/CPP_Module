0f49eb78274ee1bae853cc662e6cd1f0bf1b2b38
wkddlstn1321 <wkddlstn132@gmail.com>
2022-09-15 20:18:35 +0900

    feat : main testCase add

--- Module03/ex00/main.cpp
+++ Module03/ex00/main.cpp
@@ -8,6 +8,7 @@ int	main(void)
 	std::cout << std::endl;
 	a.setAttackDamage(5);
 	b.setAttackDamage(10);
+
 	a.attack("b");
 	std::cout << std::endl;
 	b.takeDamage(a.getAttackDamage());
@@ -22,5 +23,11 @@ int	main(void)
 	std::cout << std::endl;
 	a.takeDamage(b.getAttackDamage());
 	std::cout << std::endl;
+	std::cout << "==============a hitpoint 0 status=================" << std::endl;
 	a.beRepaired(10);
+	std::cout << std::endl;
+	for (int i = 0 ; i < 8 ; i++)
+		b.attack("a");
+	std::cout << "==============b energy 0 status=================" << std::endl;
+	b.attack("a");
 }
