8dad09902e8397d0da2f0813c63a0b13ee299edf
Insu Jang <insjang@c5r1s4.42seoul.kr>
2022-09-16 18:27:47 +0900

    virtual keyword add

--- Module03/ex01/ClapTrap.hpp
+++ Module03/ex01/ClapTrap.hpp
@@ -18,7 +18,7 @@ public:
 	~ClapTrap();
 	ClapTrap(const ClapTrap &clapTrap);
 	ClapTrap	&operator=(const ClapTrap &clapTrap);
-	void	attack(const std::string& target);
+	virtual void	attack(const std::string& target);
 	void	takeDamage(unsigned int amount);
 	void	beRepaired(unsigned int amount);
 
