1bb867c29e4c2c8e1df6db8634193da044ea227b
Insu Jang <insjang@c7r3s7.42seoul.kr>
2022-09-24 16:12:11 +0900

    a

--- Module06/ex00/main.cpp
+++ Module06/ex00/main.cpp
@@ -1,6 +1,33 @@
 #include<iostream>
 
+int	parse(std::string str)
+{
+	if (str.length() > 1)
+	{
+		while ()
+		{
+			;
+		}
+	}
+}
+
 int	main(int ac, char **av)
 {
+	if (ac != 2)
+	{
+		std::cout << "[./typeCast] [argument]" << std::endl;
+		return (1);
+	}
+	std::string	line;
+	line = static_cast<std::string>(av[1]);
+	int			check;
+
+	
+	n = line.find(".");
+	if (line.length() == 1 && )
+	
+	
+	if (n == std::string::npos && line.length() == 1)
+		;
 	
 }
